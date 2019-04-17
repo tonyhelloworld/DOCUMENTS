bc43c74e49c40bfa16beebba3e2dc9186d07a0a8
tonyhellworld <13663599793@163.com>
2019-04-17 23:10:33 +0800

    add count String lenght

---
+++ JavaScript-library/utility/countBytesLength.js
@@ -0,0 +1,13 @@
+// 计算字符串的长度
+function countBytesLength(str){
+    var length = 0
+    for(var i = 0; i< str.length ; i++){
+        // 中文日文特殊字符占两个字节
+        if(str[i].charCodeAt() >255){
+            length +=2;
+        }else{
+            length++;
+        }
+    }
+    return length;
+}
